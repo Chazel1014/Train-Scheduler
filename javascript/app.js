2d3f1dc0598e231a563a8d4c649537730e0ad135
CMarie <KickAss@Christas-MacBook-Pro.local>
2017-01-21 09:57:15 -0500

    heroku new index

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,14 @@
 swal("CHOO CHOO MFER");
 
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDwvBZ3_qCemmiHE2YBO-MQ3yO1GCVthbE",
-    authDomain: "train-schedule-e8b72.firebaseapp.com",
-    databaseURL: "https://train-schedule-e8b72.firebaseio.com",
-    storageBucket: "train-schedule-e8b72.appspot.com",
-    messagingSenderId: "249296000053"
-  };
-  
-  // firebase.initializeApp(config);
-
-
-//  //defining database 
-//   var database = firebase()
-
+ var config = {
+    apiKey: "AIzaSyBkud8kbytpaIpdmyKSKcJlxc5pfrzZVzY",
+    authDomain: "train2-b9855.firebaseapp.com",
+    databaseURL: "https://train2-b9855.firebaseio.com",
+    storageBucket: "train2-b9855.appspot.com",
+    messagingSenderId: "2951539288"
+  }
+ 
 // //push to fire base - new train is defined by input variables 
 //   database.ref().push(newTrain); 
 
@@ -29,6 +23,7 @@ swal("CHOO CHOO MFER");
 firebase.initializeApp(config); 
 
 var database = firebase.database();
+
 var trainName = "";
 var destination = "";
 var firstTrain = "";
@@ -40,14 +35,14 @@ var frequency = "";
       firstTrain = $("#firstInput").val().trim();
       frequency = $("#freqInput").val().trim();
       
-      database.ref().push({
+      var newTrain = {
           trainName: trainName,
           destination: destination,
-          firstTrain: firstTrain
-          frequency: frequency,
-
-          // event.preventDefault();     
-      })
+          firstTrain: firstTrain,
+          frequency: frequency
+   
+      }
+      database.ref().push(newTrain)
 
       return false; 
   })
